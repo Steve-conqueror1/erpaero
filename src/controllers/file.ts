9b6e2230ab663aedc6af446dc16bd6d58aec1a4c
skilonzo <stephenkilonzo22@yahoo.com>
2023-09-18 14:19:27 +0300

    Added file update endpoint for file

--- src/controllers/file.ts
+++ src/controllers/file.ts
@@ -1,88 +1,79 @@
 import { Request, Response, NextFunction } from 'express';
 import createHttpError from 'http-errors';
-import fs from "fs";
+import fs from 'fs';
 import { AppDataSource } from '../../data-source';
 import { File } from '../entities';
-import { promisify }  from 'util'
+import { promisify } from 'util';
 
-
-const unlinkAsync = promisify(fs.unlink)
+const unlinkAsync = promisify(fs.unlink);
 const fileRepository = AppDataSource.getRepository(File);
 
- const filesPath = `${process.cwd()}/uploads`;
+const filesPath = `${process.cwd()}/uploads`;
 
 export const uploadFile = async (req: Request, res: Response, next: NextFunction) => {
-  const body = req.body;
-
   const file = req.file;
   if (!file) {
     throw createHttpError(422, 'file not found');
   }
 
   try {
-      const { filename, destination, mimetype, size } = file; // название, расширение, MIME type, размер, дата загрузки;
-  const newFile = fileRepository.create({ name: filename, mimeType: mimetype, size: size, extention: file.originalname.split('.')[1] });
-  await fileRepository.save(newFile)
-    res.status(201).json(newFile)
-  }catch (error) {
-    next(error)
+    const { filename, destination, mimetype, size } = file;
+    const newFile = fileRepository.create({
+      name: filename,
+      mimeType: mimetype,
+      size: size,
+      extention: file.originalname.split('.')[1],
+    });
+    await fileRepository.save(newFile);
+    res.status(201).json(newFile);
+  } catch (error) {
+    next(error);
   }
-
 };
 
 export const deleteFile = async (req: Request, res: Response, next: NextFunction) => {
-const {id} = req.params
+  const { id } = req.params;
 
-  if(!id){
-    throw createHttpError(422, "Id is required")
+  if (!id) {
+    throw createHttpError(422, 'Id is required');
   }
 
-  try{
-      const file = await fileRepository.findOne({where: {id: Number(id)}})
+  try {
+    const file = await fileRepository.findOne({ where: { id: Number(id) } });
 
-    if(file){
-        await  fileRepository.remove(file)
+    if (file) {
+      await fileRepository.remove(file);
 
-      await unlinkAsync(`${filesPath}/${file.name}`)
-      res.status(200).json({message: "File deleted successfully"})
-    }else{
-       throw createHttpError(422, "File with that ID doesn't exist")
+      await unlinkAsync(`${filesPath}/${file.name}`);
+      res.status(200).json({ message: 'File deleted successfully' });
+    } else {
+      throw createHttpError(422, "File with that ID doesn't exist");
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getFile = async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+  if (!id) {
+    throw createHttpError(422, 'Id is required');
+  }
+  try {
+    const file = await fileRepository.findOne({ where: { id: Number(id) } });
+
+    if (!file) {
+      throw createHttpError(404, 'No such file was found');
     }
 
-
-
-  }catch (error) {
-    next(error)
+    res.status(200).json(file);
+  } catch (error) {
+    next(error);
   }
+};
 
-
-}
-
-
-export const getFile =  async (req: Request, res: Response, next: NextFunction) => {
-  const {id} = req.params
-  if(!id){
-    throw createHttpError(422, "Id is required")
-  }
-    try {
-      const file = await fileRepository.findOne({where: {id: Number(id)}})
-
-      if(!file){
-         throw createHttpError(404, "No such file was found")
-      }
-
-      res.status(200).json(file)
-    }catch (error) {
-      next(error)
-    }
-
-}
-
-export const downloadFile =  async (req: Request, res: Response, next: NextFunction) => {
-   const { id:fileName } = req.params;
-
-
-
+export const downloadFile = async (req: Request, res: Response, next: NextFunction) => {
+  const { id: fileName } = req.params;
   try {
     if (!fileName) {
       throw createHttpError(422, 'Filename is required');
@@ -99,6 +90,33 @@ export const downloadFile =  async (req: Request, res: Response, next: NextFunct
   } catch (e) {
     next(e);
   }
+};
+export const updateFile = async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+  const file = req.file;
+  if (!file) {
+    throw createHttpError(422, 'file not found');
+  }
 
-}
+  try {
+    const existingFile = await fileRepository.findOne({ where: { id: Number(id) } });
 
+    if (!existingFile) {
+      throw createHttpError(404, 'file to update not found');
+    }
+
+    await unlinkAsync(`${filesPath}/${existingFile.name}`);
+
+    const { filename, destination, mimetype, size } = file;
+    existingFile.name = filename;
+    existingFile.mimeType = mimetype;
+    existingFile.size = size;
+    existingFile.extention = file.originalname.split('.')[1];
+
+    await fileRepository.save(existingFile);
+
+    res.status(201).json(existingFile);
+  } catch (error) {
+    next(error);
+  }
+};
--- src/routes/file.ts
+++ src/routes/file.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import multer from 'multer';
-import {uploadFile, deleteFile, getFile, downloadFile} from '../controllers';
+import {uploadFile, deleteFile, getFile, downloadFile, updateFile} from '../controllers';
 
 const upload = multer({ dest: 'uploads/' });
 
@@ -10,6 +10,7 @@ router.post('/upload', upload.single('file'), uploadFile);
 router.delete('/delete/:id', deleteFile);
 router.get('/:id', getFile);
 router.get('/download/:id', downloadFile);
+router.put('/update/:id', upload.single('file'), updateFile);
 
 
 export default router;
